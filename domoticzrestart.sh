f41a1394a7294a301067a6a1066623830f04f28d
m-Aiden <46377171+m-Aiden@users.noreply.github.com>
2019-02-02 11:16:17 +0100

    Update domoticzrestart.sh

--- domoticzrestart.sh
+++ domoticzrestart.sh
@@ -5,19 +5,19 @@
 #!/bin/bash
 dt=$(date '+%d/%m/%Y %H:%M:%S')
 CONFIG=/home/domoticz/domoticz_state_checker.txt
-STATUS=`curl -s --connect-timeout 2 --max-time 5 "Accept: application/json" "http://192.168.0.177:8080/json.htm?type=devices&rid=1" | grep "status"| awk -F: '{print $2}'|sed 's/,//'| sed 's/\"//g'`
+STATUS=`curl -s --connect-timeout 2 --max-time 5 "Accept: application/json" "http://XXX.XXX.XXX.XXX:XXX/json.htm?type=devices&rid=1" | grep "status"| awk -F: '{print $2}'|sed 's/,//'| sed 's/\"//g'`
 if [ $STATUS ] ; then
    echo "Domoticz online"
    echo "$STATUS"
    exit
 else
    sleep 5
-   STATUS2=`curl -s --connect-timeout 2 --max-time 5 "Accept: application/json" "http://192.168.0.177:8080/json.htm?type=devices&rid=1" | grep "status"| awk -F: '{print $2}'|sed 's/,//'| sed 's/\"//g'`
+   STATUS2=`curl -s --connect-timeout 2 --max-time 5 "Accept: application/json" "http://XXX.XXX.XXX.XXX:XXX/json.htm?type=devices&rid=1" | grep "status"| awk -F: '{print $2}'|sed 's/,//'| sed 's/\"//g'`
    if [ $STATUS2] ; then
       exit
    else
       sleep 5
-      STATUS3=`curl -s --connect-timeout 2 --max-time 5 "Accept: application/json" "http://192.168.0.177:8080/json.htm?type=devices&rid=1" | grep "status"| awk -F: '{print $2}'|sed 's/,//'| sed 's/\"//g'`
+      STATUS3=`curl -s --connect-timeout 2 --max-time 5 "Accept: application/json" "http://XXX.XXX.XXX.XXX:XXX/json.htm?type=devices&rid=1" | grep "status"| awk -F: '{print $2}'|sed 's/,//'| sed 's/\"//g'`
       if [ $STATUS3 ] ; then
          exit
       else
