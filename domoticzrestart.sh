39c2095714a94e85f86769aad6c4438b329f9afe
m-Aiden <46377171+m-Aiden@users.noreply.github.com>
2019-02-02 16:08:20 +0100

    Update domoticzrestart.sh

--- domoticzrestart.sh
+++ domoticzrestart.sh
@@ -1,32 +1,35 @@
-# Domoticz automate restart script
-# Made by: mAiden / Ricardo
+#!/bin/bash
+# Made by mAiden / Ricardo
 # Last edit on: 02-02-2019
 
-#!/bin/bash
 dt=$(date '+%d/%m/%Y %H:%M:%S')
 CONFIG=/home/domoticz/domoticz_state_checker.txt
-STATUS=`curl -s --connect-timeout 2 --max-time 5 "Accept: application/json" "http://XXX.XXX.XXX.XXX:XXX/json.htm?type=devices&rid=1" | grep "status"| awk -F: '{print $2}'|sed 's/,//'| sed 's/\"//g'`
-if [ $STATUS ] ; then
+STATUS=`curl -s --connect-timeout 2 --max-time 5 "Accept: application/json" "http://XXX:XXX:XXX:XXX:XXX/json.htm?type=devices&rid=1" | grep "status"| awk -F: '{print $2}'|sed 's/,//'| sed 's/\"//g'`
+if [ $STATUS ]; then
    echo "Domoticz online"
    echo "$STATUS"
-   exit
+   curl -s "Accept: application/json" "http://XXX:XXX:XXX:XXX:XXX/json.htm?type=command&param=udevice&idx=idx&nvalue=0&svalue=Domoticz%20Online"
 else
    sleep 5
-   STATUS2=`curl -s --connect-timeout 2 --max-time 5 "Accept: application/json" "http://XXX.XXX.XXX.XXX:XXX/json.htm?type=devices&rid=1" | grep "status"| awk -F: '{print $2}'|sed 's/,//'| sed 's/\"//g'`
-   if [ $STATUS2] ; then
+   STATUS2=`curl -s --connect-timeout 2 --max-time 5 "Accept: application/json" "http://XXX:XXX:XXX:XXX:XXX/json.htm?type=devices&rid=1" | grep "status"| awk -F: '{print $2}'|sed 's/,//'| sed 's/\"//g'`
+   fi
+   if [ $STATUS2 ]; then
       exit
    else
       sleep 5
-      STATUS3=`curl -s --connect-timeout 2 --max-time 5 "Accept: application/json" "http://XXX.XXX.XXX.XXX:XXX/json.htm?type=devices&rid=1" | grep "status"| awk -F: '{print $2}'|sed 's/,//'| sed 's/\"//g'`
-      if [ $STATUS3 ] ; then
-         exit
+      STATUS3=`curl -s --connect-timeout 2 --max-time 5 "Accept: application/json" "http://XXX:XXX:XXX:XXX:XXX/json.htm?type=devices&rid=1" | grep "status"| awk -F: '{print $2}'|sed 's/,//'| sed 's/\"//g'`
+      if [ $STATUS3 ]; then
+	  exit
       else
          NOW=$(date +"%Y-%m-%d_%H%M%S")
-         cp /var/log/domoticz.log /home/domoticz/domoticz-$NOW.txt
+         cp /tmp/domoticz.txt /home/domoticz/domoticz-$NOW.txt
          echo "Domoticz offline"
-         #You can send a Pushover message here
+         curl --silent -u """Pushbullet apikey"":" -d type="note" -d body="Domoticz was offline. Restarted Domoticz...." -d title="Restarted Domoticz." 'https://api.pushbullet.com/v2/pushes'
          sudo service domoticz.sh restart
          echo "$NOW, Domoticz was offline. Restarted Domoticz...." >> "$CONFIG"
-      fi
-   fi
+		 sleep 10
+		 curl -s "Accept: application/json" "http://XXX:XXX:XXX:XXX:XXX/json.htm?type=command&param=udevice&idx=idx&nvalue=0&svalue=Domoticz%20Restarted"
+		 fi
+         fi
 fi
+
